c502660337b55219d1c8172e9e6bc57311777678
Mennoknaap <101333857+Mennoknaap@users.noreply.github.com>
2023-11-06 18:08:20 +0100

    Readme updated, Filter changed, added many methods

--- ProductCatalogus/Controllers/ProductController.cs
+++ ProductCatalogus/Controllers/ProductController.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,8 @@ namespace ProductCatalogus
     public class ProductController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+
+        private int _pageSize = 10;
 
         public ProductController(ApplicationDbContext context)
         {
@@ -25,43 +28,73 @@ namespace ProductCatalogus
             [FromQuery] int? PotMaatMin,
             [FromQuery] int? PotMaatMax,
             [FromQuery] string? Kleur,
-            [FromQuery] int? Pagina
+            [FromQuery] int? Pagina,
+            [FromQuery] string? Sort,
+            [FromQuery] string? SortOrder
         )
         {
-            System.Console.WriteLine(Naam);
+            List<Product> filteredProducts = _context.Products.ToList();
 
-            // Sorteer op naam
+            // Filter by name
             if (Naam != null)
             {
-                return(Ok(Filter(Naam, "Naam")));
+                 filteredProducts = Filter(Naam, product => product.Naam, filteredProducts);
             }
 
-            // Sorteer op kleur
+            // Filter by color
             if (Kleur != null)
             {
-                return Ok(Filter(Kleur, "Kleur"));
+                 filteredProducts = Filter(Naam, product => product.Kleur, filteredProducts);
             }
 
 
-            // Sorteer op potmaat
+            // Filter by potmaat
             if (PotMaatMin != null || PotMaatMax != null)
             {
-                var filteredProducts = _context.Products
+                filteredProducts = filteredProducts
                     .Where(product =>
                         (PotMaatMin == null || product.PotMaat >= PotMaatMin) &&
                         (PotMaatMax == null || product.PotMaat <= PotMaatMax))
                     .ToList();
-
-                return Ok(filteredProducts);
             }
 
-            return Ok(_context.Products);
+            // Filter by page
+            if (Pagina != null)
+            {
+                filteredProducts = filteredProducts.Skip(_pageSize * (Pagina.Value - 1)).Take(_pageSize).ToList();
+            }
+
+            // Sort
+            if (Sort != null)
+            {
+                PropertyInfo property = typeof(Product).GetProperty(Sort);
+                if (property != null)
+                {
+                    if (SortOrder != null && SortOrder.ToLower() == "desc")
+                    {
+                        filteredProducts = filteredProducts.OrderByDescending(product => property.GetValue(product, null)).ToList();
+                    }
+                    else
+                    {
+                        filteredProducts = filteredProducts.OrderBy(product => property.GetValue(product, null)).ToList();
+                    }
+                }
+                else
+                {
+                    return BadRequest("Sorteer op een bestaande property");
+                }
+            }
+
+
+            // Return all products
+            return Ok(filteredProducts);
         }
 
         //-----------------------------\\
         //-------------POST------------\\
         //-----------------------------\\
 
+        // Add a new product
         [HttpPost("PostProduct")]
         public IActionResult Post(
             [FromBody] Product product
@@ -70,14 +103,22 @@ namespace ProductCatalogus
             // Create a new Code with 13 random characters
             product.Code = RandomString(13);
 
-            // TODO: Check if the product already exists
+            // Check if the productcode already exists
+            while (_context.Products.Find(product.Code) != null)
+            {
+                product.Code = RandomString(13);
+            }
 
+            if (product.Naam.Length > 50)
+            {
+                return BadRequest("De naam mag niet langer zijn dan 50 karakters");
+            }
 
             product.Kleur = product.Kleur ?? "Geen kleur";
             _context.Products.Add(product);
 
             _context.SaveChanges();
-            return Ok();
+            return Ok("Product met code " + product.Code + " is toegevoegd");
         }
 
 
@@ -86,10 +127,58 @@ namespace ProductCatalogus
         //-------------PUT-------------\\
         //-----------------------------\\
 
+        // Change a product by code
+        [HttpPut("PutProduct")]
+        public IActionResult Put(
+            [FromQuery] string code,
+            [FromBody] Product product
+        )
+        {
+            var productToChange = _context.Products.Find(code);
+
+            if (productToChange == null)
+            {
+                return NotFound("Product niet gevonden");
+            }
+            
+            productToChange.Naam = product.Naam;
+            productToChange.PotMaat = product.PotMaat;
+            productToChange.PlantHoogte = product.PlantHoogte;
+            productToChange.Kleur = product.Kleur;
+            productToChange.ProductGroep = product.ProductGroep;
+
+            _context.SaveChanges();
+            return Ok("Product met code " + code + " is aangepast");
+        }
+
+
         //-----------------------------\\
         //------------DELETE-----------\\
         //-----------------------------\\
 
+        // Delete a product by code
+        [HttpDelete("DeleteProduct")]
+        public IActionResult Delete(
+            [FromQuery] string code
+        )
+        {
+            var productToDelete = _context.Products.Find(code);
+
+            if (productToDelete == null)
+            {
+                return NotFound("Product niet gevonden");
+            }
+
+            _context.Products.Remove(productToDelete);
+            _context.SaveChanges();
+            return Ok("Product met code " + code + " is verwijderd");
+        }
+
+
+
+        //-----------------------------\\
+        //-----------METHODS-----------\\
+        //-----------------------------\\
 
         // Create a random string with a given size
         private string RandomString(int v)
@@ -105,23 +194,9 @@ namespace ProductCatalogus
 
 
         // Filter the products on String
-        private List<Product> Filter(string propertyName, string value)
+        private List<Product> Filter<T>(string value, Func<Product, T> propertySelector, List<Product> products)
         {
-            var products = _context.Products.AsQueryable();
-
-            if (propertyName == "Naam")
-            {
-                products = products.Where(product => product.Naam.Contains(value));
-            }
-            else if (propertyName == "Kleur")
-            {
-                products = products.Where(product => product.Kleur.Contains(value));
-            }
-
-            return products.ToList();
+            return products.Where(product => propertySelector(product).ToString().Contains(value)).ToList();
         }
-
-
-
     }
 }
--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # Productcatalogus-API
 ## Keuzes die ik heb gemaakt bij het maken van de API
 ### 1. De database
-Ik heb ervoor gekozen om voor de database een EF-Core ORM database te gebruiken. In het specifiek SQLite. Ik gebruik deze, omdat het een kleinschalige applicatie zal worden en het ook niet nodig zal zijn om sql queries zoals JOIN te gebruiken. Daarnaast is het ook makkelijk om de database te maken en te gebruiken.
+Ik heb ervoor gekozen om voor de database een EF-Core ORM database te gebruiken. In het specifiek SQLite. Ik gebruik SQLite, omdat ik hier al ervaring mee heb en omdat het makkelijk is om mee te werken. Ook is het makkelijk om te testen, omdat je geen database hoeft te installeren. Je kan gewoon een database file maken en die gebruiken.
 
 ### 2. Filteren
-Ik heb ervoor gekozen om het met een FromQuery te doen, omdat...
+Ik heb ervoor gekozen om de filtervariabelen binnen te krijgen via FromQuery's. Ik heb hiervoor gekozen, omdat het makkelijk is om te gebruiken en omdat het makkelijk is om te testen.
 
-## Wat ik zou doen als ik meer tijd had
+### 3. Duplicate code
+Ik heb de filter code van kleur en naam in 1 methode gezet, omdat dit eerst 2 behoorlijk dezelfde soort code was.
+
+### 4. De API
+Ik heb de HTTP status codes gebruikt die ik vond dat het beste bij de situatie paste. Op deze manier kan je er makkelijk achter komen wat er mis is gegaan.
+
+## Wat ik zou doen als ik meer tijd erin zou steken
 ### 1. De database
+Als ik meer tijd erin zou steken, zou ik meer onderzoek doen naar welke soort database voor deze applicatie handig zou zijn.
+
+### 2. Authentication en Authorization
+Als ik meer tijd erin zou steken, zou ik ook een authentication en authorization systeem maken. Dit zou ik of met JWT tokens of role based accounts. Ik zou dit doen, omdat het dan veiliger is en omdat je dan ook kan zien wie wat heeft gedaan.
+
+### 3. Unit tests
+Als laatst zou ik ook nog unit tests toevoegen, om te er zo zeker mogelijk van te zijn dat er geen fouten in de code zitten.
